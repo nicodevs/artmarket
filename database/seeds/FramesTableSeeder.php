751d4b6a77f0f9a37b7c818cdbd9249ebf66b56c
Nico Beta <nico1485@gmail.com>
2017-10-10 15:17:23 -0300

    Fix frames seeder

--- database/seeds/FramesTableSeeder.php
+++ database/seeds/FramesTableSeeder.php
@@ -14,18 +14,24 @@ class FramesTableSeeder extends Seeder
         DB::table('frames')->insert([
             [
                 'name' => 'Marco negro',
+                'description' => '',
                 'thumbnail' => 'white.jpg',
-                'border' => 'white_border.png'
+                'border' => 'white_border.png',
+                'border_mobile' => 'white_border.png'
             ],
             [
                 'name' => 'Marco madera',
+                'description' => '',
                 'thumbnail' => 'wood.jpg',
-                'border' => 'wood_border.png'
+                'border' => 'wood_border.png',
+                'border_mobile' => 'wood_border.png'
             ],
             [
                 'name' => 'Marco blanco',
+                'description' => '',
                 'thumbnail' => 'black.jpg',
-                'border' => 'black_border.png'
+                'border' => 'black_border.png',
+                'border_mobile' => 'black_border.png'
             ]
         ]);
     }
